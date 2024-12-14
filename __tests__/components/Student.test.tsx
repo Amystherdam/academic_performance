be36933874cad8f826df719e491feabfb60a3986
Lucas de Amystherdam <pro.lucas07@gmail.com>
2024-12-14 14:03:01 -0300

    create alias to project paths

--- __tests__/components/Student.test.tsx
+++ __tests__/components/Student.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import Student from "../../src/components/Student";
+import Student from "@components/Student";
 
 describe("Student Component", () => {
   it("renders the student name passed as a prop", () => {
--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,23 @@
 import type { Config } from 'jest';
+import * as aliases from "./jsconfig.aliases.json"
+
+const buildAliases = () => {
+  const { paths, baseUrl } = aliases.compilerOptions;
+  const pathObject = {};
+
+  for (const path in paths) {
+    pathObject[`^${path.replace("/*", "/(.*)")}$`] =
+      `<rootDir>/${baseUrl}/${paths[path]}`.replace("/*", "/$1");
+  }
+
+  return pathObject;
+};
 
 const config: Config = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    ...buildAliases(),
+  },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./setupTests.ts'],
 };
