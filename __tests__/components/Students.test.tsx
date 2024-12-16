b242de09b9ffbb589906fd4c879ea182f7d70008
Amystherdam <43969809+Amystherdam@users.noreply.github.com>
2024-12-16 03:05:49 -0300

    Revert "Implement unit tests"

--- __tests__/components/Students.test.tsx
+++ __tests__/components/Students.test.tsx
@@ -1,7 +1,6 @@
 import { render, waitFor } from "@testing-library/react";
 import Students from "@components/Students";
 import api from "@services/Api";
-import { AxiosError, AxiosHeaders } from "axios";
 
 const mockNavigate = jest.fn();
 jest.mock("react-router", () => ({
@@ -43,30 +42,12 @@ describe("Students Component", () => {
   });
 
   it("handles API errors", async () => {
-    // Criando um erro Axios com a configuração correta
-    const request = { path: "/foo" };
-    const headers = new AxiosHeaders();
-    const config = {
-      url: "http://localhost:3000",
-      headers,
-    };
-    const error = new AxiosError("Boom!", "ESOMETHING", config, request, {
-      status: 200,
-      data: { errors: [{ title: "API Error" }] },
-      statusText: "ok",
-      config,
-      headers,
-    });
-
-    // Mockando o erro da API corretamente
-    (api.get as jest.Mock).mockRejectedValueOnce(error);
+    (api.get as jest.Mock).mockRejectedValueOnce(new Error("API Error"));
 
     const { findByText, getByText } = render(<Students />);
 
-    // Verifica se a mensagem de "loading" está presente
     expect(getByText(/loading/i)).toBeInTheDocument();
 
-    // Verifica se a mensagem de erro "API Error" é exibida corretamente
-    expect(await findByText(/API Error/i)).toBeInTheDocument();
+    expect(await findByText(/No students registered/i)).toBeInTheDocument();
   });
 });
--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -18,9 +18,9 @@ export default function MainContainer() {
       <NavBar paths={navBarlinks} />
 
       <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
-        <main className="flex flex-1 overflow-hidden" aria-label="main">
+        <main className="flex flex-1 overflow-hidden">
           <section
-            aria-label="section"
+            aria-labelledby="primary-heading"
             className="flex h-full min-w-0 flex-1 flex-col overflow-y-auto lg:order-last"
           >
             <Outlet />
